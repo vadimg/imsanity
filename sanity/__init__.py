028871038a9348884f21a7f711f787dd2f88ef52
Vadim Graboys <dimva13@gmail.com>
2016-02-04 00:46:46 -0500

    init

---
+++ sanity/__init__.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+script_dir = sys.path[0]
+
+if not script_dir:
+    # loaded interactively, assume cwd is the "script dir"
+    script_dir = os.getcwd()
+
+cur_dir = os.path.abspath(script_dir)
+
+# loop until we get to filesystem root
+while cur_dir != os.path.dirname(cur_dir):
+    if os.path.isfile(os.path.join(cur_dir, '.importsanity')):
+        sys.path.append(cur_dir)
+        break
+    cur_dir = os.path.abspath(os.path.join(cur_dir, os.pardir))
