ebce5455bec95a69a698ffa559a43ec7fa6d1b42
Vadim Graboys <dimva13@gmail.com>
2016-02-04 01:07:18 -0500

    change .importsanity -> .imsanity

--- imsanity/__init__.py
+++ imsanity/__init__.py
@@ -11,7 +11,7 @@ cur_dir = os.path.abspath(script_dir)
 
 # loop until we get to filesystem root
 while cur_dir != os.path.dirname(cur_dir):
-    if os.path.isfile(os.path.join(cur_dir, '.importsanity')):
+    if os.path.isfile(os.path.join(cur_dir, '.imsanity')):
         sys.path.append(cur_dir)
         break
     cur_dir = os.path.abspath(os.path.join(cur_dir, os.pardir))
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='imsanity',
     packages=['imsanity'],  # this must be the same as the name above
-    version='0.1',
-    description='Import sanity. Adds the first parent directory containing .importsanity to PYTHONPATH.',
+    version='0.2',
+    description='Import sanity. Adds the first parent directory containing .imsanity to PYTHONPATH.',
     author='Vadim Graboys',
     author_email='dimva13@gmail.com',
     url='https://github.com/vadimg/imsanity',
-    download_url='https://github.com/vadimg/imsanity/tarball/0.1',
+    download_url='https://github.com/vadimg/imsanity/tarball/0.2',
     keywords=['import', 'sanity', 'imsanity'],
     classifiers=[],
 )
