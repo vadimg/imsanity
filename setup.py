a2f79b33dd3c4a4783a5c3363acf251e2d92d3ed
Vadim Graboys <dimva13@gmail.com>
2016-02-04 01:00:23 -0500

    change name to imsanity

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-    name='sanity',
-    packages=['sanity'],  # this must be the same as the name above
+    name='imsanity',
+    packages=['imsanity'],  # this must be the same as the name above
     version='0.1',
-    description='Import sanity. Adds the first parent directory containing .importsanity to PYTHONPATH',
+    description='Import sanity. Adds the first parent directory containing .importsanity to PYTHONPATH.',
     author='Vadim Graboys',
     author_email='dimva13@gmail.com',
-    url='https://github.com/vadimg/sanity',
-    download_url='https://github.com/vadimg/sanity/tarball/0.1',
-    keywords=['import', 'sanity'],
+    url='https://github.com/vadimg/imsanity',
+    download_url='https://github.com/vadimg/imsanity/tarball/0.1',
+    keywords=['import', 'sanity', 'imsanity'],
     classifiers=[],
 )
