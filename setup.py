d92e6e0de5b14da4c9a0423ee203c56aa6cb99c9
Vadim Graboys <dimva13@gmail.com>
2016-02-04 01:12:10 -0500

    bump version to 0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='imsanity',
     packages=['imsanity'],  # this must be the same as the name above
-    version='0.2',
+    version='0.3',
     description='Import sanity. Adds the first parent directory containing .imsanity to PYTHONPATH.',
     author='Vadim Graboys',
     author_email='dimva13@gmail.com',
     url='https://github.com/vadimg/imsanity',
-    download_url='https://github.com/vadimg/imsanity/tarball/0.2',
+    download_url='https://github.com/vadimg/imsanity/tarball/0.3',
     keywords=['import', 'sanity', 'imsanity'],
     classifiers=[],
 )
